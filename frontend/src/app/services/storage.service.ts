dda84db3d044a5ad450e41f2719af3e851158ea9
marcelnoehre <marcelnoehre@web.de>
2023-12-15 17:09:57 +0100

    storage service

---
+++ frontend/src/app/services/storage.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+	providedIn: 'root'
+})
+export class StorageService {
+	public getLocalEntry(key: string): any {
+		return JSON.parse(localStorage.getItem(key)!);
+	}
+
+	public setLocalEntry(key: string, data: unknown): void {
+		localStorage.setItem(key, JSON.stringify(data));
+	}
+
+	public deleteLocalEntry(key: string): void {
+		localStorage.removeItem(key);
+	}
+
+	public clearLocal(): void {
+		localStorage.clear();
+	}
+
+	public isLocalEmpty(): boolean {
+		return localStorage.length === 0;
+	}
+
+	public getSessionEntry(key: string): any {
+		return JSON.parse(sessionStorage.getItem(key)!);
+	}
+
+	public setSessionEntry(key: string, data: unknown): void {
+		sessionStorage.setItem(key, JSON.stringify(data));
+	}
+
+	public deleteSessionEntry(key: string): void {
+		sessionStorage.removeItem(key);
+	}
+
+	public clearSession(): void {
+		sessionStorage.clear();
+	}
+
+	public isSessionEmpty(): boolean {
+		return sessionStorage.length === 0;
+	}
+
+	public clearAll(): void {
+		this.clearLocal();
+		this.clearSession();
+	}
+} 
