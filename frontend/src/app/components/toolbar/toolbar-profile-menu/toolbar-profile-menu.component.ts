05663dbc523af1e0457d03e5ebb6fe7016f5249c
marcelnoehre <marcelnoehre@web.de>
2024-01-05 17:13:47 +0100

    handle database update on logout

--- frontend/src/app/components/toolbar/toolbar-profile-menu/toolbar-profile-menu.component.ts
+++ frontend/src/app/components/toolbar/toolbar-profile-menu/toolbar-profile-menu.component.ts
@@ -1,7 +1,10 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatMenu } from '@angular/material/menu';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { ApiService } from 'src/app/services/api/api.service';
 import { EventService } from 'src/app/services/event.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
 import { StorageService } from 'src/app/services/storage.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -19,15 +22,30 @@ export class ToolbarProfileMenuComponent {
     private router: Router,
 		private storage: StorageService, 
 		private event: EventService,
-		private user: UserService
+		private user: UserService,
+		private api: ApiService,
+		private snackbar: SnackbarService,
+		private translate: TranslateService
 	) {
 		this.fullName = this.getFullName();
 	}
 
 	public logout(): void {
-    	this.storage.deleteSessionEntry('user');
-		this.user.user = this.storage.getSessionEntry('user');
-    	this.router.navigateByUrl('/login');
+		this.api.logout(this.user.token, this.user.username).subscribe(
+			(response) => {
+				this.storage.deleteSessionEntry('user');
+				this.user.user = this.storage.getSessionEntry('user');
+				this.snackbar.open(this.translate.instant(response.message));
+				this.router.navigateByUrl('/login');
+			},
+			(error) => {
+				if (error.status === 403) {
+					this.storage.clearSession();
+					this.router.navigateByUrl('/login');
+				}
+				this.snackbar.open(this.translate.instant(error.error.message));
+			}
+		);
 	}
 
 	public getFullName() {
