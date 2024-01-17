bc6e9dc73c8e75fc26616582cdc1e5cc8b62f76c
marcelnoehre <marcelnoehre@web.de>
2024-01-17 23:40:32 +0100

    adjust task interface

--- frontend/src/app/interfaces/data/task.ts
+++ frontend/src/app/interfaces/data/task.ts
@@ -2,8 +2,9 @@ export interface Task {
     uid: string,
     author: string,
     project: string,
-    state: string,
     title: string,
     description: string,
+    assigned: string,
+    state: string,
     order: number
 }
