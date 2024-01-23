868dbf75ecd1913d0bc218f3174ed3dc5b4211ac
marcelnoehre <marcelnoehre@web.de>
2024-01-23 10:23:36 +0100

    import a list of tasks

--- frontend/src/app/components/import-tasks/import-tasks.component.ts
+++ frontend/src/app/components/import-tasks/import-tasks.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./import-tasks.component.scss']
 })
 export class ImportTasksComponent {
+  taskList: any
 
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length) {
+      const file = input.files[0];
+      const fileExtension = file.name.split('.').pop()?.toLowerCase();
+      if(['json', 'xml', 'yaml'].includes(fileExtension || '')) {
+        const reader = new FileReader;
+        reader.onload = (e) => {
+            const result = e.target?.result as string;
+            this.taskList = JSON.parse(atob(result.split(',')[1]));
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+  }
 }
