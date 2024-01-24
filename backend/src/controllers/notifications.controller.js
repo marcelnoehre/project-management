72b2ab3290ef5bf2d8a591240994e45816c69baa
marcelnoehre <marcelnoehre@web.de>
2024-01-24 01:31:38 +0100

    retrieve notifications data

--- backend/src/controllers/notifications.controller.js
+++ backend/src/controllers/notifications.controller.js
@@ -2,4 +2,34 @@ const admin = require('firebase-admin');
 const jwt = require('jsonwebtoken');
 const db = admin.firestore();
 
-module.exports = { };
+async function getNotifications(req, res, next) {
+    try {
+        const notificationsCollection = db.collection('notifications');
+        const unseenQuery = notificationsCollection
+            .where('project', '==', req.body.project)
+            .where('unseen', 'array-contains', req.body.username)
+            .get();
+        const seenQuery = notificationsCollection
+            .where('project', '==', req.body.project)
+            .where('seen', 'array-contains', req.body.username)
+            .get();
+        const notifiactions = [];
+        unseenQuery.forEach(doc => {
+            const data = doc.data();
+            console.log(data);
+            notifiactions.push({});
+        });
+        seenQuery.forEach(doc => {
+            const data = doc.data();
+            console.log(data);
+            notifiactions.push({});
+        });
+        res.json(tasks);
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports = {
+    getNotifications
+};
--- backend/src/routes/notifications.js
+++ backend/src/routes/notifications.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/notifications.controller');
+const notificationsController = require('../controllers/notifications.controller');
 const { jwtAuth } = require('../auth/jwtAuth');
+
+router.post('/getNotifications', jwtAuth, notificationsController.getNotifications);
 
 module.exports = router;
