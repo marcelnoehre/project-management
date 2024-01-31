8c16c87175b1607d50c410a718bb91109385e7b7
marcelnoehre <marcelnoehre@web.de>
2024-01-31 18:12:37 +0100

    update frontend interfaces

---
+++ frontend/src/app/interfaces/data/user-stats.ts
@@ -0,0 +1,9 @@
+export interface UserStats {
+    created: number,
+    imported: number,
+    edited: number,
+    trashed: number,
+    restored: number,
+    deleted: number,
+    cleared: number
+}
