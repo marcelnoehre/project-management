e8707af34797959d8753ef7b695de681cc7dd6cf
marcelnoehre <marcelnoehre@web.de>
2024-01-31 20:49:39 +0100

    create stats function templates

--- backend/src/controllers/stats.controller.js
+++ backend/src/controllers/stats.controller.js
@@ -9,6 +9,87 @@ async function optimize(req, res, next) {
     }
 }
 
+async function projectStats(req, res, next) {
+    try {
+
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function memberAmount(req, res, next) {
+    try {
+
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function userActivity(req, res, next) {
+    try {
+
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function statLeaders(req, res, next) {
+    try {
+
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function taskProgress(req, res, next) {
+    try {
+
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function averageTime(req, res, next) {
+    try {
+
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function taskAmount(req, res, next) {
+    try {
+
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function trashStats(req, res, next) {
+    try {
+
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function wip(req, res, next) {
+    try {
+
+    } catch (err) {
+        next(err);
+    }
+}
+
 module.exports = {
-    optimize
+    optimize,
+    projectStats,
+    memberAmount,
+    userActivity,
+    statLeaders,
+    taskProgress,
+    averageTime,
+    taskAmount,
+    trashStats,
+    wip
 };
--- backend/src/routes/stats.js
+++ backend/src/routes/stats.js
@@ -4,5 +4,14 @@ const statsController = require('../controllers/stats.controller');
 const { jwtAuth } = require('../auth/jwtAuth');
 
 router.post('/optimize', jwtAuth, statsController.optimize);
+router.post('/projectStats', jwtAuth, statsController.projectStats);
+router.post('/memberAmount', jwtAuth, statsController.memberAmount);
+router.post('/userActivity', jwtAuth, statsController.userActivity);
+router.post('/statLeaders', jwtAuth, statsController.statLeaders);
+router.post('/taskProgress', jwtAuth, statsController.taskProgress);
+router.post('/averageTime', jwtAuth, statsController.averageTime);
+router.post('/taskAmount', jwtAuth, statsController.taskAmount);
+router.post('/trashStats', jwtAuth, statsController.trashStats);
+router.post('/wip', jwtAuth, statsController.wip);
 
 module.exports = router;
