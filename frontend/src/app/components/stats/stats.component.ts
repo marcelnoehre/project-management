7696c0abbc5b3abce48bd8d88d4844fcb43aaa18
marcelnoehre <marcelnoehre@web.de>
2024-02-02 16:59:44 +0100

    load stats data from storage to remove traffic from firebase

--- frontend/src/app/components/stats/stats.component.ts
+++ frontend/src/app/components/stats/stats.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProgressBarMode } from '@angular/material/progress-bar';
 import { StatsService } from 'src/app/services/stats.service';
 
 @Component({
@@ -8,6 +9,7 @@ import { StatsService } from 'src/app/services/stats.service';
 })
 export class StatsComponent implements OnInit {
   information = 'INIT';
+  mode: ProgressBarMode = 'determinate';
   loading = 0;
   data: any = {
     optimizeOrder: null,
@@ -20,6 +22,17 @@ export class StatsComponent implements OnInit {
     taskProgress: null,
     projectRoadmap: null
   }
+  statLabels = ['created', 'imported', 'updated', 'edited', 'trashed', 'restored', 'deleted', 'cleared'];
+  icons: any = {
+    created: 'add',
+    imported: 'upload_file',
+    updated: 'view_kanban',
+    edited: 'edit',
+    trashed: 'delete',
+    restored: 'undo',
+    deleted: 'delete_forever',
+    cleared: 'clear'
+  }
 
   constructor(
     private stats: StatsService
@@ -31,10 +44,18 @@ export class StatsComponent implements OnInit {
     await new Promise<void>(done => setTimeout(() => done(), 500));
     this.stats.init();
     this.stats.getUpdateSubject().subscribe(async (res) => {
-      await new Promise<void>(done => setTimeout(() => done(), 500));
-      this.loading = res.percentage * 100;
-      this.information = res.information;
-      this.data[res.step] = res.data;
+      if (res.step === 'storage') {
+        this.data = res.data;
+        this.information = res.information;
+        this.mode = 'indeterminate';
+        await new Promise<void>(done => setTimeout(() => done(), 2000));
+        this.loading = res.percentage * 100;
+      } else {
+        await new Promise<void>(done => setTimeout(() => done(), 500));
+        this.loading = res.percentage * 100;
+        this.information = res.information;
+        this.data[res.step] = res.data;
+      }
     });
   }
 
--- frontend/src/app/services/stats.service.ts
+++ frontend/src/app/services/stats.service.ts
@@ -3,50 +3,87 @@ import { ApiService } from './api/api.service';
 import { UserService } from './user.service';
 import { Observable, Subject } from 'rxjs';
 import { Loading } from '../interfaces/loading';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatsService {
   private updateSubject = new Subject<Loading>();
-  private calculated: boolean = false;
+  private data: any = {
+    optimizeOrder: null,
+    personalStats: null,
+    stats: null,
+    statLeaders: null,
+    taskAmount: null,
+    averageTime: null,
+    wip: null,
+    taskProgress: null,
+    projectRoadmap: null
+  }
   
   constructor(
     private api: ApiService,
-    private user: UserService
+    private user: UserService,
+    private storage: StorageService
   ) { }
 
-  init() {    
-    if (!this.calculated) {
+  async init() {
+    this.data = {
+      optimizeOrder: null,
+      personalStats: null,
+      stats: null,
+      statLeaders: null,
+      taskAmount: null,
+      averageTime: null,
+      wip: null,
+      taskProgress: null,
+      projectRoadmap: null
+    }
+    if (this.storage.getSessionEntry('statsRetrieval') === true) {
+      await new Promise<void>(done => setTimeout(() => done(), 500));
+      this.submitUpdate({ step: 'storage', information: 'STORAGE', percentage: 1, data: this.storage.getSessionEntry('stats') });
+    } else {
       this.submitUpdate({ step: 'init', information: 'OPTIMIZE_ORDER', percentage: 1/9, data: null });
       const token = this.user.token;
       this.api.optimizeOrder(token).subscribe(
         (response) => {
+          this.data['optimizeOrder'] = response;
           this.submitUpdate({ step: 'optimizeOrder', information: 'PERSONAL_STATS', percentage: 1/9, data: response });
           this.api.personalStats(token).subscribe(
             (response) => {
+              this.data['personalStats'] = response;
               this.submitUpdate({ step: 'personalStats', information: 'STATS', percentage: 2/9, data: response });
               this.api.stats(token).subscribe(
                 (response) => {
+                  this.data['stats'] = response;
                   this.submitUpdate({ step: 'stats', information: 'STAT_LEADERS', percentage: 3/9, data: response });
                   this.api.statLeaders(token).subscribe(
                     (response) => {
+                      this.data['statLeaders'] = response;
                       this.submitUpdate({ step: 'statLeaders', information: 'TASK_AMOUNT', percentage: 4/9, data: response });
                       this.api.taskAmount(token).subscribe(
                         (response) => {
+                          this.data['taskAmount'] = response;
                           this.submitUpdate({ step: 'taskAmount', information: 'AVERAGE_TIME', percentage: 5/9, data: response });
                           this.api.averageTime(token).subscribe(
                             (response) => {
+                              this.data['averageTime'] = response;
                               this.submitUpdate({ step: 'averageTime', information: 'WIP', percentage: 6/9, data: response });
                               this.api.wip(token).subscribe(
                                 (response) => {
+                                  this.data['wip'] = response;
                                   this.submitUpdate({ step: 'wip', information: 'TASK_PROGRESS', percentage: 7/9, data: response });
                                   this.api.taskProgress(token).subscribe(
                                     (response) => {
+                                      this.data['taskProgress'] = response;
                                       this.submitUpdate({ step: 'taskProgress', information: 'PROJECT_ROADMAP', percentage: 8/9, data: response });
                                       this.api.projectRoadmap(token).subscribe(
                                         (response) => {
+                                          this.data['projectRoadmap'] = response;
                                           this.submitUpdate({ step: 'projectRoadmap', information: 'DONE', percentage: 9/9, data: response });
+                                          this.storage.setSessionEntry('stats', this.data);
+                                          this.storage.setSessionEntry('statsRetrieval', true);
                                         },
                                         (error) => { }
                                       );
