919b51275523666875262a6814637fe6ad822301
marcelnoehre <marcelnoehre@web.de>
2024-02-09 16:04:08 +0100

    refactor stats initialization

--- frontend/src/app/services/stats.service.ts
+++ frontend/src/app/services/stats.service.ts
@@ -46,91 +46,19 @@ export class StatsService {
       await new Promise<void>(done => setTimeout(() => done(), 500));
       this.submitUpdate({ step: 'storage', information: 'STORAGE', percentage: 1, data: this.storage.getSessionEntry('stats') });
     } else {
-      this.submitUpdate({ step: 'init', information: 'OPTIMIZE_ORDER', percentage: 1/9, data: null });
       const token = this.user.token;
-      this.api.optimizeOrder(token).subscribe(
-        (response) => {
-          this.data['optimizeOrder'] = response;
-          this.submitUpdate({ step: 'optimizeOrder', information: 'PERSONAL_STATS', percentage: 1/9, data: response });
-          this.api.personalStats(token).subscribe(
-            (response) => {
-              this.data['personalStats'] = response;
-              this.submitUpdate({ step: 'personalStats', information: 'STATS', percentage: 2/9, data: response });
-              this.api.stats(token).subscribe(
-                (response) => {
-                  this.data['stats'] = response;
-                  this.submitUpdate({ step: 'stats', information: 'STAT_LEADERS', percentage: 3/9, data: response });
-                  this.api.statLeaders(token).subscribe(
-                    (response) => {
-                      this.data['statLeaders'] = response;
-                      this.submitUpdate({ step: 'statLeaders', information: 'TASK_AMOUNT', percentage: 4/9, data: response });
-                      this.api.taskAmount(token).subscribe(
-                        (response) => {
-                          this.data['taskAmount'] = response;
-                          this.submitUpdate({ step: 'taskAmount', information: 'AVERAGE_TIME', percentage: 5/9, data: response });
-                          this.api.averageTime(token).subscribe(
-                            (response) => {
-                              this.data['averageTime'] = response;
-                              this.submitUpdate({ step: 'averageTime', information: 'WIP', percentage: 6/9, data: response });
-                              this.api.wip(token).subscribe(
-                                (response) => {
-                                  this.data['wip'] = response;
-                                  this.submitUpdate({ step: 'wip', information: 'TASK_PROGRESS', percentage: 7/9, data: response });
-                                  this.api.taskProgress(token).subscribe(
-                                    (response) => {
-                                      this.data['taskProgress'] = response;
-                                      this.submitUpdate({ step: 'taskProgress', information: 'PROJECT_ROADMAP', percentage: 8/9, data: response });
-                                      this.api.projectRoadmap(token).subscribe(
-                                        (response) => {
-                                          this.data['projectRoadmap'] = response;
-                                          this.submitUpdate({ step: 'projectRoadmap', information: 'DONE', percentage: 9/9, data: response });
-                                          this.storage.setSessionEntry('stats', this.data);
-                                          this.storage.setSessionEntry('statsRetrieval', true);
-                                        },
-                                        (error) => {
-                                          this._error.handleApiError(error);
-                                        }
-                                      );
-                                    },
-                                    (error) => {
-                                      this._error.handleApiError(error);
-                                    }
-                                  );
-                                },
-                                (error) => {
-                                  this._error.handleApiError(error);
-                                }
-                              );
-                            },
-                            (error) => {
-                              this._error.handleApiError(error);
-                            }
-                          );
-                        },
-                        (error) => {
-                          this._error.handleApiError(error);
-                        }
-                      );
-                    },
-                    (error) => {
-                      this._error.handleApiError(error);
-                    }
-                  );
-                },
-                (error) => {
-                  this._error.handleApiError(error);
-                }
-              );
-            },
-            (error) => {
-              this._error.handleApiError(error);
-            }
-          );
-        },
-        (error) => {
-          this._error.handleApiError(error);
-        }
-      );
+      this.submitUpdate({ step: 'init', information: 'OPTIMIZE_ORDER', percentage: 0/9, data: null });
+      this.submitUpdate({ step: 'optimizeOrder', information: 'PERSONAL_STATS', percentage: 1/9, data: await this.optimizeOrder(token) });
+      this.submitUpdate({ step: 'personalStats', information: 'STATS', percentage: 2/9, data: await this.personalStats(token) });
+      this.submitUpdate({ step: 'stats', information: 'STAT_LEADERS', percentage: 3/9, data: await this.stats(token) });
+      this.submitUpdate({ step: 'statLeaders', information: 'TASK_AMOUNT', percentage: 4/9, data: await this.statLeaders(token) });
+      this.submitUpdate({ step: 'taskAmount', information: 'AVERAGE_TIME', percentage: 5/9, data: await this.taskAmount(token) });
+      this.submitUpdate({ step: 'averageTime', information: 'WIP', percentage: 6/9, data: await this.averageTime(token) });
+      this.submitUpdate({ step: 'wip', information: 'TASK_PROGRESS', percentage: 7/9, data: await this.wip(token) });
+      this.submitUpdate({ step: 'taskProgress', information: 'PROJECT_ROADMAP', percentage: 8/9, data: await this.taskProgress(token) });
+      this.submitUpdate({ step: 'projectRoadmap', information: 'DONE', percentage: 9/9, data: await this.projectRoadmap(token) });
+      this.storage.setSessionEntry('stats', this.data);
+      this.storage.setSessionEntry('statsRetrieval', true);
     }
   }
 
