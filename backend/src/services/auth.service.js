6f620e0db7094407f60f0f28152c816b2da6004a
marcelnoehre <marcelnoehre@web.de>
2024-02-09 23:54:51 +0100

    documentation for auth service

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,9 +1,26 @@
+/**
+ * Validates a password.
+ *
+ * @param {Object} db - Firestore instance.
+ * @param {string} username - The username.
+ * @param {string} password - The password.
+ *
+ * @returns {boolean} Whether the password is valid.
+ */
 async function passwordValid(db, username, password) {
     const passwordsCollection = db.collection('passwords');
     const passwordsSnapshot = await passwordsCollection.where('username', '==', username).get();
     return (passwordsSnapshot.size === 1 && passwordsSnapshot.docs[0].data().password === password);
 }
 
+/**
+ * Returns a single user.
+ *
+ * @param {Object} db - Firestore instance.
+ * @param {string} username - The username.
+ *
+ * @returns {Object | null} The user data, if the username is unique.
+ */
 async function singleUser(db, username) {
     const usersCollection = db.collection('users');
     const usersSnapshot = await usersCollection.where('username', '==', username).get();
@@ -14,12 +31,31 @@ async function singleUser(db, username) {
     }
 }
 
+/**
+ * Checks whether a user already exists.
+ *
+ * @param {Object} db - Firestore instance.
+ * @param {string} username - The username.
+ *
+ * @returns {boolean} Whether the user doesn't already exist.
+ */
 async function isNewUser(db, username) {
     const usersCollection = db.collection('users');
     const usersSnapshot = await usersCollection.where('username', '==', username).get();
     return usersSnapshot.empty;
 }
 
+/**
+ * Create a user.
+ *
+ * @param {Object} db - Firestore instance.
+ * @param {string} username - The username.
+ * @param {string} fullName - The full name.
+ * @param {string} language - The language.
+ * @param {string} password - The password.
+ *
+ * @returns {void}
+ */
 async function createUser(db, username, fullName, language, password) {
     const userData = {
         username: username,
@@ -52,6 +88,16 @@ async function createUser(db, username, fullName, language, password) {
     await Promise.all(promises);
 }
 
+/**
+ * Update a user attribute.
+ *
+ * @param {Object} db - Firestore instance.
+ * @param {string} username - The username.
+ * @param {string} attribute - The attribute that should get updated.
+ * @param {string} value - The new value for the attribute.
+ *
+ * @returns {boolean} Whether the attribute was updated successfully.
+ */
 async function updateAttribute(db, username, attribute, value) {
     const validAttributes = ['username', 'fullName', 'language', 'initials', 'color', 'profilePicture', 'password'];
     if (validAttributes.includes(attribute)) {
@@ -70,6 +116,16 @@ async function updateAttribute(db, username, attribute, value) {
     }
 }
 
+/**
+ * Update the password data.
+ *
+ * @param {Object} db - Firestore instance.
+ * @param {string} username - The username.
+ * @param {string} attribute - The attribute that should get updated.
+ * @param {string} value - The new value for the attribute.
+ *
+ * @returns {boolean} Whether the password data has been updated successfully.
+ */
 async function updatePassword(db, username, attribute, value) {
     const passwordsCollection = db.collection('passwords');
     const passwordsSnapshot = await passwordsCollection.where('username', '==', username).get();
@@ -79,6 +135,16 @@ async function updatePassword(db, username, attribute, value) {
     return passwordsSnapshot.size === 1;
 }
 
+/**
+ * Update the user data.
+ *
+ * @param {Object} db - Firestore instance.
+ * @param {string} username - The username.
+ * @param {string} attribute - The attribute that should get updated.
+ * @param {string} value - The new value for the attribute.
+ *
+ * @returns {boolean} Whether the user data has been updated successfully.
+ */
 async function updateUser(db, username, attribute, value) {
     const usersCollection = db.collection('users');
     const usersSnapshot = await usersCollection.where('username', '==', username).get();
@@ -88,6 +154,14 @@ async function updateUser(db, username, attribute, value) {
     return usersSnapshot.size === 1;
 }
 
+/**
+ * Delete a user.
+ *
+ * @param {Object} db - Firestore instance.
+ * @param {string} username - The username.
+ *
+ * @returns {boolean} Whether the user has been deleted successfully.
+ */
 async function deleteUser(db, username) {
     try {
         const usersCollection = db.collection('users');
@@ -104,32 +178,49 @@ async function deleteUser(db, username) {
     }
 }
 
-function defaultColor() {
-    const defaultColors = ['#FF0000', '#00FF00', '#0000FF', '#FFA500', '#FFFFFF'];
-    return defaultColors[Math.floor(Math.random() * defaultColors.length)];
-}
-
-function generateInitials(fullName) {
-    const fullNameArr = fullName.split(/\s+/);
-    const first = fullNameArr[0].charAt(0).toUpperCase();
-    const second = fullNameArr.length > 1 ? fullNameArr[1]?.charAt(0).toUpperCase() : ''
-    return first + second;
-}
-
+/**
+ * Update the user stats.
+ *
+ * @param {Object} db - Firestore instance.
+ * @param {string} username - The username.
+ * @param {string} attribute - The attribute that should get updated.
+ * @param {number} counter - The amount that should get added to the attribute counter.
+ *
+ * @returns {void}
+ */
 async function updateUserStats(db, username, attribute, counter) {
     const usersCollection = db.collection('users');
     const usersSnapshot = await usersCollection.where('username', '==', username).get();
     updateStats(usersSnapshot, attribute, counter);
 }
 
+/**
+ * Update the project stats.
+ *
+ * @param {Object} db - Firestore instance.
+ * @param {string} username - The project.
+ * @param {string} attribute - The attribute that should get updated.
+ * @param {number} counter - The amount that should get added to the attribute counter.
+ *
+ * @returns {void}
+ */
 async function updateProjectStats(db, project, attribute, counter) {
     const projectsCollection = db.collection('projects');
     const projectsSnapshot = await projectsCollection.where('name', '==', project).get();
     updateStats(projectsSnapshot, attribute, counter);
 }
 
+/**
+ * Update a specific stat counter.
+ *
+ * @param {Object} snapshot - Firestore snapshot.
+ * @param {string} attribute - The attribute that should get updated.
+ * @param {number} counter - The amount that should get added to the attribute counter.
+ *
+ * @returns {void}
+ */
 async function updateStats(snapshot, attribute, counter) {
-    if (!snapshot.empty) {
+    if (snapshot.size === 1) {
         const doc = snapshot.docs[0];
         const stats = doc.data().stats;
         stats[attribute] = stats[attribute] + counter;
@@ -137,6 +228,30 @@ async function updateStats(snapshot, attribute, counter) {
             stats: stats
         });
     }
+}
+
+/**
+ * Get a random default color.
+ *
+ * @returns {string} The color.
+ */
+function defaultColor() {
+    const defaultColors = ['#FF0000', '#00FF00', '#0000FF', '#FFA500', '#FFFFFF'];
+    return defaultColors[Math.floor(Math.random() * defaultColors.length)];
+}
+
+/**
+ * Generate initials based on the full name.
+ *
+ * @param {string} fullName - The full name.
+ *
+ * @returns {string} The generated initials.
+ */
+function generateInitials(fullName) {
+    const fullNameArr = fullName.split(/\s+/);
+    const first = fullNameArr[0].charAt(0).toUpperCase();
+    const second = fullNameArr.length > 1 ? fullNameArr[1]?.charAt(0).toUpperCase() : ''
+    return first + second;
 }
 
 module.exports = { 
