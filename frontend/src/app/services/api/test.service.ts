06bf3ca49686fec12ad97b5a8a91a2795880fd58
marcelnoehre <marcelnoehre@web.de>
2024-02-15 20:15:07 +0100

    test service auth calls

--- frontend/src/app/services/api/test.service.ts
+++ frontend/src/app/services/api/test.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { Stats } from 'mocha';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Permission } from 'src/app/enums/permission.enum';
 import { AssignedStats } from 'src/app/interfaces/data/assigned-stats';
 import { CategoryStats } from 'src/app/interfaces/data/category-stats';
 import { Progress } from 'src/app/interfaces/data/progress';
 import { ProjectRoadmap } from 'src/app/interfaces/data/project-roadmap';
+import { Response } from 'src/app/interfaces/data/response';
 import { StatLeaders } from 'src/app/interfaces/data/stat-leaders';
 import { State } from 'src/app/interfaces/data/state';
 import { Task } from 'src/app/interfaces/data/task';
@@ -17,35 +19,305 @@ import { User } from 'src/app/interfaces/data/user';
 })
 export class TestService {
 
-  constructor() { }
+  constructor(
+	private translate: TranslateService
+  ) { }
 
 	// ### AUTH ###
 	public verify(token: string): Observable<User> {
-    throw Error('Method not implemented!');
+		switch (token) {
+			case 'owner':
+				return of({
+					token: 'owner',
+					username: 'owner',
+					fullName: 'Mock Owner',
+					initials: 'MO',
+					color: '#FFFFFF',
+					language: 'en',
+					project: 'MockProject',
+					permission: Permission.OWNER,
+					profilePicture: '',
+					notificationsEnabled: true,
+					isLoggedIn: true,
+					stats: {
+						created: 91,
+						imported: 10,
+						updated: 45,
+						edited: 78,
+						trashed: 32,
+						restored: 57,
+						deleted: 23,
+						cleared: 69
+					}
+				});
+			case 'admin':
+				return of({
+					token: 'admin',
+					username: 'admin',
+					fullName: 'Mock Admin',
+					initials: 'MA',
+					color: '#FFFFFF',
+					language: 'en',
+					project: 'MockProject',
+					permission: Permission.ADMIN,
+					profilePicture: '',
+					notificationsEnabled: true,
+					isLoggedIn: true,
+					stats: {
+						created: 42,
+						imported: 15,
+						updated: 78,
+						edited: 63,
+						trashed: 29,
+						restored: 51,
+						deleted: 94,
+						cleared: 12
+					}
+				});
+			case 'member':
+				return of({
+					token: 'member',
+					username: 'member',
+					fullName: 'Mock Member',
+					initials: 'MM',
+					color: '#FFFFFF',
+					language: 'de',
+					project: 'MockProject',
+					permission: Permission.MEMBER,
+					profilePicture: '',
+					notificationsEnabled: true,
+					isLoggedIn: true,
+					stats: {
+						created: 64,
+						imported: 27,
+						updated: 89,
+						edited: 14,
+						trashed: 50,
+						restored: 73,
+						deleted: 3,
+						cleared: 67
+					}
+				});
+			case 'invited':
+				return of({
+					token: 'invited',
+					username: 'invited',
+					fullName: 'Mock Invited',
+					initials: 'MI',
+					color: '#FFFFFF',
+					language: 'en',
+					project: 'MockProject',
+					permission: Permission.INVITED,
+					profilePicture: '',
+					notificationsEnabled: true,
+					isLoggedIn: true,
+					stats: {
+						created: 77,
+						imported: 42,
+						updated: 19,
+						edited: 56,
+						trashed: 83,
+						restored: 5,
+						deleted: 38,
+						cleared: 91
+					}
+				});
+			case 'none':
+				return of({
+					token: 'none',
+					username: 'none',
+					fullName: 'Mock None',
+					initials: 'MN',
+					color: '#FFFFFF',
+					language: 'en',
+					project: 'MockProject',
+					permission: Permission.NONE,
+					profilePicture: '',
+					notificationsEnabled: true,
+					isLoggedIn: true,
+					stats: {
+						created: 0,
+						imported: 0,
+						updated: 0,
+						edited: 0,
+						trashed: 0,
+						restored: 0,
+						deleted: 0,
+						cleared: 0
+					}
+				});
+			default:
+				throw new Error(this.translate.instant('ERROR.INVALID_TOKEN'));
+		}
 	}
 
 	public refreshToken(token: string): Observable<string> {
-    throw Error('Method not implemented!');
+		if (['owner', 'admin', 'member', 'invited', 'none'].includes(token)) {
+			return of(token);
+		} else {
+			throw new Error(this.translate.instant('ERROR.INVALID_TOKEN'));
+		}
 	}
 
 	public login(username: string, password: string): Observable<User> {
-    throw Error('Method not implemented!');
+		switch (username) {
+			case 'owner':
+				return of({
+					token: 'owner',
+					username: 'owner',
+					fullName: 'Mock Owner',
+					initials: 'MO',
+					color: '#FFFFFF',
+					language: 'en',
+					project: 'MockProject',
+					permission: Permission.OWNER,
+					profilePicture: '',
+					notificationsEnabled: true,
+					isLoggedIn: true,
+					stats: {
+						created: 91,
+						imported: 10,
+						updated: 45,
+						edited: 78,
+						trashed: 32,
+						restored: 57,
+						deleted: 23,
+						cleared: 69
+					}
+				});
+			case 'admin':
+				return of({
+					token: 'admin',
+					username: 'admin',
+					fullName: 'Mock Admin',
+					initials: 'MA',
+					color: '#FFFFFF',
+					language: 'en',
+					project: 'MockProject',
+					permission: Permission.ADMIN,
+					profilePicture: '',
+					notificationsEnabled: true,
+					isLoggedIn: true,
+					stats: {
+						created: 42,
+						imported: 15,
+						updated: 78,
+						edited: 63,
+						trashed: 29,
+						restored: 51,
+						deleted: 94,
+						cleared: 12
+					}
+				});
+			case 'member':
+				return of({
+					token: 'member',
+					username: 'member',
+					fullName: 'Mock Member',
+					initials: 'MM',
+					color: '#FFFFFF',
+					language: 'de',
+					project: 'MockProject',
+					permission: Permission.MEMBER,
+					profilePicture: '',
+					notificationsEnabled: true,
+					isLoggedIn: true,
+					stats: {
+						created: 64,
+						imported: 27,
+						updated: 89,
+						edited: 14,
+						trashed: 50,
+						restored: 73,
+						deleted: 3,
+						cleared: 67
+					}
+				});
+			case 'invited':
+				return of({
+					token: 'invited',
+					username: 'invited',
+					fullName: 'Mock Invited',
+					initials: 'MI',
+					color: '#FFFFFF',
+					language: 'en',
+					project: 'MockProject',
+					permission: Permission.INVITED,
+					profilePicture: '',
+					notificationsEnabled: true,
+					isLoggedIn: true,
+					stats: {
+						created: 77,
+						imported: 42,
+						updated: 19,
+						edited: 56,
+						trashed: 83,
+						restored: 5,
+						deleted: 38,
+						cleared: 91
+					}
+				});
+			case 'none':
+				return of({
+					token: 'none',
+					username: 'none',
+					fullName: 'Mock None',
+					initials: 'MN',
+					color: '#FFFFFF',
+					language: 'en',
+					project: 'MockProject',
+					permission: Permission.NONE,
+					profilePicture: '',
+					notificationsEnabled: true,
+					isLoggedIn: true,
+					stats: {
+						created: 0,
+						imported: 0,
+						updated: 0,
+						edited: 0,
+						trashed: 0,
+						restored: 0,
+						deleted: 0,
+						cleared: 0
+					}
+				});
+			default:
+				throw new Error(this.translate.instant('ERROR.INVALID_TOKEN'));
+		}
 	}
 
 	public register(username: string, fullName: string, language: string, password: string): Observable<Response> {
-    throw Error('Method not implemented!');
+		if (username === '') {
+			return of({
+				message: 'SUCCESS.REGISTRATION'
+			});
+		} else {
+			throw new Error('ERROR.REGISTRATION');
+		}
 	}
 
 	public updateUser(token: string, attribute: string, value: string): Observable<Response> {
-    throw Error('Method not implemented!');
+		return of({
+			message: 'SUCCESS.UPDATE_ACCOUNT'
+		});
 	}
 
 	public toggleNotifications(token: string, notificationsEnabled: boolean): Observable<Response> {
-    throw Error('Method not implemented!');
+		if (notificationsEnabled) {
+			return of({
+				message: 'SUCCESS.NOTIFICATIONS_ON'
+			});
+		} else {
+			return of({
+				message: 'SUCCESS.NOTIFICATIONS_OFF'
+			});
+		}
 	}
 
 	public deleteUser(token: string): Observable<Response> {
-    throw Error('Method not implemented!');
+    	return of({
+			message: 'SUCCESS.DELETE_ACCOUNT'
+		});
 	}
 
 
