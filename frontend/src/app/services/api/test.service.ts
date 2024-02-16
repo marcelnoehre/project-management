204d7750e9e2d4ef4f6c5db6eea63475d3380849
marcelnoehre <marcelnoehre@web.de>
2024-02-16 09:41:47 +0100

    test service stats calls

--- frontend/src/app/services/api/test.service.ts
+++ frontend/src/app/services/api/test.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { Stats } from 'mocha';
 import { Observable, of } from 'rxjs';
 import { Permission } from 'src/app/enums/permission.enum';
 import { TaskState } from 'src/app/enums/task-state.enum';
@@ -12,6 +11,7 @@ import { ProjectRoadmap } from 'src/app/interfaces/data/project-roadmap';
 import { Response } from 'src/app/interfaces/data/response';
 import { StatLeaders } from 'src/app/interfaces/data/stat-leaders';
 import { State } from 'src/app/interfaces/data/state';
+import { Stats } from 'src/app/interfaces/data/stats';
 import { Task } from 'src/app/interfaces/data/task';
 import { TaskProgress } from 'src/app/interfaces/data/task-progress';
 import { User } from 'src/app/interfaces/data/user';
@@ -105,39 +105,257 @@ export class TestService {
 
   	// ### STATS ###
 	public personalStats(token: string): Observable<Stats> {
-    throw Error('Method not implemented!');
+		return of({
+			deleted: 1,
+			edited: 1,
+			created: 3,
+			imported: 77,
+			restored: 1,
+			updated: 20,
+			cleared: 1,
+			trashed: 9
+		});
 	}
 
 	public stats(token: string): Observable<AssignedStats[]> {
-    throw Error('Method not implemented!');
+		return of([
+			{
+				id: 'STATS.PROJECT',
+				stats: {
+					deleted: 5,
+					edited: 4,
+					created: 4,
+					imported: 104,
+					restored: 17,
+					updated: 29,
+					cleared: 7,
+					trashed: 16
+				}
+			},
+			{
+				id: 'owner',
+				stats: {
+					deleted: 1,
+					edited: 1,
+					created: 3,
+					imported: 77,
+					restored: 1,
+					updated: 20,
+					cleared: 1,
+					trashed: 9
+				}
+			},
+			{
+				id: 'admin',
+				stats: {
+					deleted: 0,
+					edited: 0,
+					created: 3,
+					imported: 11,
+					restored: 7,
+					updated: 2,
+					cleared: 0,
+					trashed: 2
+				}
+			},
+			{
+				id: 'member',
+				stats: {
+					deleted: 1,
+					edited: 2,
+					created: 3,
+					imported: 14,
+					restored: 5,
+					updated: 7,
+					cleared: 6,
+					trashed: 4
+				}
+			},
+			{
+				id: 'STATS.OTHERS',
+				stats: {
+					deleted: 3,
+					edited: 1,
+					created: 5,
+					imported: 2,
+					restored: 4,
+					updated: 0,
+					cleared: 0,
+					trashed: 1
+				}
+			}
+		]);
 	}
 
 	public statLeaders(token: string): Observable<StatLeaders> {
-    throw Error('Method not implemented!');
+		return of({
+			created: {
+				username: ['owner', 'admin'],
+				value: 3
+			},
+			imported: {
+				username: ['owner'],
+				value: 77
+			},
+			updated: {
+				username: ['admin'],
+				value: 20
+			},
+			edited: {
+				username: ['member'],
+				value: 1
+			},
+			trashed: {
+				username: ['member'],
+				value: 9
+			},
+			restored: {
+				username: ['owner'],
+				value: 1
+			},
+			deleted: {
+				username: ['admin'],
+				value: 2
+			},
+			cleared: {
+				username: ['admin'],
+				value: 1
+			}
+		});
 	}
 
 	public taskAmount(token: string): Observable<CategoryStats> {
-    throw Error('Method not implemented!');
+		return of({
+			NONE: 5,
+			TODO: 7,
+			PROGRESS: 2,
+			REVIEW: 6,
+			DONE: 10,
+			DELETED: 3
+		});
 	}
 
 	public averageTime(token: string): Observable<CategoryStats> {
-    throw Error('Method not implemented!');
+		return of({
+			NONE: 138785180.7142857,
+			TODO: 151694419.625,
+			PROGRESS: 161779809,
+			REVIEW: 161779452.66666666,
+			DONE: 145641722.8,
+			DELETED: 0
+		});
 	}
 
 	public wip(token: string): Observable<number> {
-    throw Error('Method not implemented!');
+		return of(2);
 	}
 
 	public taskProgress(token: string): Observable<TaskProgress> {
-    throw Error('Method not implemented!');
+		return of({
+			timestamps: [
+			  1707706711326,
+			  1707706711547,
+			  1707706711761,
+			  1707706711969,
+			  1707706712166,
+			  1707706712367,
+			  1707706712566,
+			  1707706712767,
+			  1707706712945,
+			  1707706713118,
+			  1707706713301,
+			  1707706984404,
+			  1707706984558,
+			  1707706985141,
+			  1707706985455,
+			  1707706985601,
+			  1707949317400,
+			  1707949317586,
+			  1707949317799,
+			  1707949318000,
+			  1707949318194,
+			  1707949318388,
+			  1707949318578,
+			  1707949318752,
+			  1707949318942,
+			  1707949319137,
+			  1707949319299
+			],
+			NONE: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27],
+			TODO: [0,0,1,2,3,4,5,6,7,8,9,9,10,11,12,13,13,13,14,15,16,17,18,19,20,21,22],
+			PROGRESS: [0,0,0,0,0,1,2,3,4,5,6,6,6,7,8,9,9,9,9,9,9,10,11,12,13,14,15],
+			REVIEW: [0,0,0,0,0,0,0,1,2,3,4,4,4,4,5,6,6,6,6,6,6,6,6,7,8,9,10],
+			DONE: [0,0,0,0,0,0,0,0,0,1,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,4,5]
+		});
 	}
 
 	public projectRoadmap(token: string): Observable<ProjectRoadmap[]> {
-    throw Error('Method not implemented!');
+		return of([
+			{
+			  type: 'STATS.PROJECT_ROADMAP.CREATED',
+			  timestamp: 1707688306528,
+			  username: 'owner',
+			  target: null
+			},
+			{
+			  type: 'STATS.PROJECT_ROADMAP.INVITED',
+			  timestamp: 1707688391791,
+			  username: 'owner',
+			  target: 'member'
+			},
+			{
+			  type: 'STATS.PROJECT_ROADMAP.JOINED',
+			  timestamp: 1707688399967,
+			  username: 'member',
+			  target: null
+			},
+			{
+			  type: 'STATS.PROJECT_ROADMAP.REMOVED',
+			  timestamp: 1707688526728,
+			  username: 'owner',
+			  target: 'member'
+			},
+			{
+			  type: 'STATS.PROJECT_ROADMAP.INVITED',
+			  timestamp: 1707688529525,
+			  username: 'owner',
+			  target: 'admin'
+			},
+			{
+			  type: 'STATS.PROJECT_ROADMAP.JOINED',
+			  timestamp: 1707688539092,
+			  username: 'admin',
+			  target: null
+			},
+			{
+			  type: 'STATS.PROJECT_ROADMAP.INVITED',
+			  timestamp: 1707696589097,
+			  username: 'admin',
+			  target: 'member'
+			},
+			{
+			  type: 'STATS.PROJECT_ROADMAP.JOINED',
+			  timestamp: 1707696596223,
+			  username: 'member',
+			  target: null
+			},
+			{
+			  type: 'STATS.PROJECT_ROADMAP.LEFT',
+			  timestamp: 1707696880346,
+			  username: 'member',
+			  target: null
+			},
+			{
+			  type: 'STATS.PROJECT_ROADMAP.REMOVED',
+			  timestamp: 1707697212830,
+			  username: 'owner',
+			  target: 'member'
+			}
+		]);
 	}
 
 	public optimizeOrder(token: string): Observable<Response> {
-    throw Error('Method not implemented!');
+		return this.response('SUCCESS.STATS.OPTIMIZE');
 	}
 
 
