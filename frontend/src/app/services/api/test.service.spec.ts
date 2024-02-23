ef9d9db15aea66cc0d8e207b447a385d519776ea
marcelnoehre <marcelnoehre@web.de>
2024-02-23 17:39:03 +0100

    fix unit testing setup

--- frontend/src/app/services/api/test.service.spec.ts
+++ frontend/src/app/services/api/test.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TestService } from './test.service';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 describe('TestService', () => {
   let service: TestService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot()],
+      providers: [
+				{ provide: TranslateService, useClass: TranslateService }
+			]
+    });
     service = TestBed.inject(TestService);
   });
 
