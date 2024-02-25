5cc1aa5efe2b80ca9290e9909a50024ddfdc7713
marcelnoehre <marcelnoehre@web.de>
2024-02-25 23:17:47 +0100

    first test case for login

---
+++ backend/src/specs/auth.spec.ts
@@ -0,0 +1,69 @@
+import { Request, Response, NextFunction } from 'express';
+import * as authService from '../services/auth.service';
+import * as jwt from 'jsonwebtoken';
+import * as auth from '../controllers/auth.controller';
+
+jest.mock('../services/auth.service');
+jest.mock('jsonwebtoken');
+jest.mock('firebase-admin', () => ({
+    initializeApp: jest.fn(),
+    firestore: jest.fn(),
+}));
+
+const user = {
+    username: "owner",
+    fullName: "Mock Owner",
+    initials: "MO",
+    color: "#FFFFFF",
+    language: "en",
+    project: "MockProject",
+    permission: "OWNER",
+    profilePicture: "",
+    notificationsEnabled: true,
+    isLoggedIn: true,
+    stats: {
+        created: 91,
+        imported: 10,
+        updated: 45,
+        edited: 78,
+        trashed: 32,
+        restored: 57,
+        deleted: 23,
+        cleared: 69
+    }
+}
+
+describe('auth controller', () => {
+    const req = {
+        body: {
+            username: 'testuser',
+            password: 'testpassword',
+        },
+    } as Request;
+
+    const res = {
+        json: jest.fn(),
+        status: jest.fn(() => res),
+        send: jest.fn(),
+    } as unknown as Response;
+
+    const next = jest.fn() as NextFunction;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe('login', () => {
+        it('should login successfully', async () => {
+            authService.passwordValid.mockResolvedValue(true);
+            authService.singleUser.mockResolvedValue(user);
+            await auth.login(req, res, next);
+            expect(jwt.sign).toHaveBeenCalledWith(user, '3R#q!ZuFb2sPn8yT^@5vLmN7jA*C6hG', { expiresIn: '1h' });
+            expect(res.json).toHaveBeenCalledWith(user);
+            expect(res.status).not.toHaveBeenCalled();
+            expect(res.send).not.toHaveBeenCalled();
+            expect(next).not.toHaveBeenCalled();
+        });
+    });
+
+});
