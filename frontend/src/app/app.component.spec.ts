3d4371dd081dd2b188789f9d090139e84a341012
marcelnoehre <marcelnoehre@web.de>
2024-02-26 00:37:59 +0100

    unit tests app.component

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -22,15 +22,22 @@ describe('AppComponent', () => {
 		fixture.detectChanges();
 	});
 
-	it('should load test environment', () => {
-		expect(environment.environement).toBe('test');
+	describe('setup', () => {
+		it('should load test environment', () => {
+			expect(environment.environement).toBe('test');
+		});
+	
+		it('should create the app', () => {
+			expect(component).toBeTruthy();
+		});
+	
+		it('should have as title: frontend', () => {
+			expect(component.title).toEqual('frontend');
+		});
 	});
 
-	it('should create the app', () => {
-		expect(component).toBeTruthy();
-	});
-
-	it('should have as title: frontend', () => {
-		expect(component.title).toEqual('frontend');
-	});
+	it('should initialize TranslateService with default language', () => {
+		const translateService: TranslateService = component['_translate'];
+		expect(translateService.getDefaultLang()).toBe(environment.defaultLanguage);
+	  });
 });
