9b46008b53abb1f24f5a16a856e5044ef2919f2f
marcelnoehre <marcelnoehre@web.de>
2024-03-05 18:56:00 +0100

    unit tests for dashboard component

--- frontend/src/app/components/dashboard/dashboard.component.spec.ts
+++ frontend/src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,28 +1,53 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
 import { environment } from 'src/environments/environment';
 import { AppModule } from 'src/app/app.module';
+import { CreateTaskComponent } from '../create-task/create-task.component';
+import { ROUTES, Router, RouterModule } from '@angular/router';
 
 describe('DashboardComponent', () => {
+	const mockRoutes = [
+		{
+			path: '',
+			component: DashboardComponent
+		},
+		{
+			path: 'task/create',
+			component: CreateTaskComponent
+		}
+	];
+	let router: Router;
 	let component: DashboardComponent;
 	let fixture: ComponentFixture<DashboardComponent>;
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
-			imports: [AppModule],
-			declarations: [DashboardComponent]
+			imports: [AppModule, RouterModule.forRoot(mockRoutes)],
+			declarations: [DashboardComponent],
+			providers: [
+				{ provide: ROUTES, multi: true, useValue: [] }
+			]
 		});
 		fixture = TestBed.createComponent(DashboardComponent);
+		router = TestBed.inject(Router);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
 	});
 
-	it('should load test environment', () => {
-		expect(environment.environement).toBe('test');
+	describe('setup', () => {
+		it('should load test environment', () => {
+			expect(environment.environement).toBe('test');
+		});
+	
+		it('should create', () => {
+			expect(component).toBeTruthy();
+		});
 	});
 
-	it('should create', () => {
-		expect(component).toBeTruthy();
-	});
+	it('should redirect the user', fakeAsync(() => {
+		component.redirectTo('/task/create');
+		tick();
+		expect(router.url).toBe('/task/create');
+	}));
 });
